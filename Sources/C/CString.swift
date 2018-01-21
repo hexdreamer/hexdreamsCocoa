344e826f7dc7d94249d0ac62ee65006af4d55cef
Kenny Leung <kenny_leung@pobox.com>
2018-01-20 18:33:20 -0800

    Fixed warning from 4.1 compiler.

--- Sources/C/CString.swift
+++ Sources/C/CString.swift
@@ -8,8 +8,9 @@ extension C {
     // Copies data
     // https://stackoverflow.com/questions/27455773/converting-a-c-char-array-to-a-string
     public static func string<T>(_ value:inout T) -> String {
+        let capacity = MemoryLayout.size(ofValue:value)
         return withUnsafePointer(to:&value) {
-            $0.withMemoryRebound(to:UInt8.self, capacity:MemoryLayout.size(ofValue:value)) {
+            $0.withMemoryRebound(to:UInt8.self, capacity:capacity) {
                 String(cString:$0)
             }
         }
