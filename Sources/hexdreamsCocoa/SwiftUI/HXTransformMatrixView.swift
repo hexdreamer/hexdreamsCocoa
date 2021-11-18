0682d2bdd8e8c125fa4cadf7f3b4ab9465304745
Zach Young <zacharysyoung@gmail.com>
2021-11-17 19:49:54 -0800

    big changes: dynamic sizing; gutted FormattedValue

--- Sources/hexdreamsCocoa/SwiftUI/HXTransformMatrixView.swift
+++ Sources/hexdreamsCocoa/SwiftUI/HXTransformMatrixView.swift
@@ -7,16 +7,13 @@
 
 import SwiftUI
 
-struct FormattedValue: Identifiable {
-    let id = UUID()
-    let name: String
+struct FormattedValue {
     let strVal: String
 
     var integral: String = "0"
     var decimal: String = ""
 
-    init(name: String, strVal: String) {
-        self.name = name
+    init(_ strVal: String) {
         self.strVal = strVal
 
         let parts: [String] = strVal.split(separator: ".").map { String($0) }
@@ -43,16 +40,80 @@ extension HorizontalAlignment {
 
 public struct HXTransformMatrixView: View {
     let tr: CGAffineTransform
+    let title: String
     let maxSigFigs: Int
     let roundErrCutoff: CGFloat
 
-    public init(_ tr:CGAffineTransform, maxSigFigs:Int = 4, roundErrCutoff:CGFloat = 1/10e6) {
+    var formattedValues: [FormattedValue]
+    var frameW: CGFloat
+
+    public init(_ tr:CGAffineTransform, title:String = "", maxSigFigs:Int = 4, roundErrCutoff:CGFloat = 1/10e6) {
         self.tr = tr
-        self.maxSigFigs = maxSigFigs
+        self.maxSigFigs = maxSigFigs < 10 ? maxSigFigs : 10
         self.roundErrCutoff = roundErrCutoff
+        self.title = title
+
+        // hack around "'self' used before all stored properties are initialized" error ...
+        self.formattedValues = [FormattedValue]()
+        self.frameW = 200
+
+        // ... 'cause I really want to do this
+        let (fmtdVals, maxLen) = self.makeFormattedVals()
+        self.formattedValues = fmtdVals
+
+        let widths: [CGFloat] = [200, 213, 226, 239, 252, 265, 278, 291, 304, 317, 330]
+        let wIdx = maxLen < 10 ? maxLen-1 : 10
+        self.frameW = widths[wIdx]
     }
 
-    var formattedValues: [FormattedValue] {
+    public var body: some View {
+        HStack {
+            Text("\(title)")
+            HStack {
+                Spacer()
+
+                VStack(alignment: .integral) {
+                    let a = formattedValues[0]
+                    let c = formattedValues[2]
+                    let tx = formattedValues[4]
+
+                    formattedValueView(a)
+                    formattedValueView(c)
+                    formattedValueView(tx)
+                }
+
+                Spacer()
+
+                VStack(alignment: .integral) {
+                    let b = formattedValues[1]
+                    let d = formattedValues[3]
+                    let ty = formattedValues[5]
+
+                    formattedValueView(b)
+                    formattedValueView(d)
+                    formattedValueView(ty)
+                }
+
+                Spacer()
+
+                VStack(alignment: .integral) {
+                    let i0 = FormattedValue("0")
+                    let i1 = FormattedValue("0")
+                    let i2 = FormattedValue("1")
+
+                    formattedValueView(i0)
+                    formattedValueView(i1)
+                    formattedValueView(i2)
+                }
+
+                Spacer()
+
+            }
+            .frame(width: frameW, height: 100)
+        }
+    }
+
+    func makeFormattedVals() -> ([FormattedValue], Int)  {
         let fmt = NumberFormatter()
         fmt.numberStyle = .decimal
         fmt.usesSignificantDigits = true
@@ -62,65 +123,30 @@ public struct HXTransformMatrixView: View {
         let vals: [String] = [tr.a, tr.b, tr.c, tr.d, tr.tx, tr.ty]
             .map {
                 let clamped = abs($0) < roundErrCutoff ? 0 : $0
-                print(clamped)
                 return fmt.string(for: clamped)!
             }
 
-        return [
-            FormattedValue(name: "a", strVal: vals[0]),
-            FormattedValue(name: "b", strVal: vals[1]),
-            FormattedValue(name: "c", strVal: vals[2]),
-            FormattedValue(name: "d", strVal: vals[3]),
-            FormattedValue(name: "tx", strVal: vals[4]),
-            FormattedValue(name: "ty", strVal: vals[5]),
+        let fmtdVals: [FormattedValue] = [
+            FormattedValue(vals[0]),  //  a
+            FormattedValue(vals[1]),  //  b
+            FormattedValue(vals[2]),  //  c
+            FormattedValue(vals[3]),  //  d
+            FormattedValue(vals[4]),  // tx
+            FormattedValue(vals[5]),  // ty
         ]
-    }
 
-    public var body: some View {
-        HStack {
-            Spacer()
-
-            VStack(alignment: .integral) {
-                let a = formattedValues[0]
-                let c = formattedValues[2]
-                let tx = formattedValues[4]
-
-                formattedValue(a)
-                formattedValue(c)
-                formattedValue(tx)
-            }
-
-            Spacer()
-
-            VStack(alignment: .integral) {
-                let b = formattedValues[1]
-                let d = formattedValues[3]
-                let ty = formattedValues[5]
-
-                formattedValue(b)
-                formattedValue(d)
-                formattedValue(ty)
-            }
-
-            Spacer()
-
-            VStack(alignment: .integral) {
-                let i0 = FormattedValue(name: "i0", strVal: "0")
-                let i1 = FormattedValue(name: "i1", strVal: "0")
-                let i2 = FormattedValue(name: "i2", strVal: "1")
-
-                formattedValue(i0)
-                formattedValue(i1)
-                formattedValue(i2)
-            }
-
-            Spacer()
-
+        // Find the longest string (to determine frame width)
+        var maxLen = 0
+        for fmtdVal in fmtdVals {
+            let il = fmtdVal.integral.count
+            let dl = fmtdVal.decimal.count
+            maxLen = max(dl+il, maxLen)
         }
-        .frame(width: 230, height: 100)
+
+        return (fmtdVals, maxLen)
     }
 
-    func formattedValue(_ fm: FormattedValue) -> some View {
+    func formattedValueView(_ fm: FormattedValue) -> some View {
         return HStack {
             Text("\(fm.integral)")
                 .alignmentGuide(.integral) { d in d[HorizontalAlignment.trailing]}
@@ -137,20 +163,17 @@ public struct HXTransformMatrixView: View {
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         let identity = CGAffineTransform.identity
-        VStack {
-            Spacer()
+        VStack(alignment: .leading) {
             HXTransformMatrixView(identity)
-            Spacer()
-            HXTransformMatrixView(identity
-                                    .scaledBy(x: 1.3, y: 2.4)
-                                    .rotated(by: 23.8)
-            )
-            Spacer()
-            HXTransformMatrixView(identity
-                                    .translatedBy(x: 100, y: 201.18)
-                                    .scaledBy(x: 0.4, y: 0.003)
-                                  )
-            Spacer()
+
+            let t = identity
+                .scaledBy(x: 1.000003, y: 2.00000004)
+                .rotated(by: 23.8)
+
+            ForEach(0...11, id: \.self) { i in
+                HXTransformMatrixView(t, title: "\(i) sig figs", maxSigFigs: i)
+            }
         }
+        .previewLayout(.fixed(width: 500, height: 1600))
     }
 }
